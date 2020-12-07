7a38d3abd1145b2e8b4766bec5c766d3537486cc
Rocco <rp@Roccos-iMac.local>
2020-12-07 10:40:39 -0500

    playing sound with some controls

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 let notesPlayed = [] // An empty array (will hold our music)
+let masterVolume = 0.5
 
-let btnA = document.querySelector(`#btnA`)
-let btnB = document.querySelector(`#btnB`)
+
+let btnA = document.querySelector(`#tom1`)
+let btnB = document.querySelector(`#tom2`)
 let btnReplay = document.querySelector(`#replay`)
+let rngVol = document.querySelector(`#volume`)
 
 let clickedA = function() {
-	notesPlayed.push(`A`)  // Add A to the history
-	// Play the A note
+  let sound = new Audio(`audio/tom1.wav`)
+  sound.volume = masterVolume
+  sound.play()
+  notesPlayed.push(sound)  // Add A to the history
 }
 let clickedB = function() {
-	notesPlayed.push(`B`)  // Add B to the history
-	// Play the B note
+  let sound = new Audio(`audio/tom2.wav`)
+  sound.volume = masterVolume
+  sound.play()
+	notesPlayed.push(sound)  // Add B to the history
 }
 let replayNotes = function() {
-	let playOneNote = function(note) {
-		// Play the audio for the note
-		console.log(`Playing note: ${note}`)
-	}
-	notesPlayed.forEach(playOneNote)
+  let index = 0  // counter
+  
+	let playOneNote = function() {
+    let note = notesPlayed[index]
+    let speed = document.querySelector(`#speed`).value
+
+		note.volume = masterVolume
+    note.play() // Play the audio for the note
+
+    index = index + 1
+
+    // Only if there are still notes to play
+    if (index < notesPlayed.length) {
+      // Play a function at a 500ms delay
+      setTimeout(playOneNote, speed) // Loop this function again, in one second
+    }
+  }
+
+  playOneNote() // just like a forEach, except it's timed
+	//notesPlayed.forEach(playOneNote)
 }
+
+let updateVolume = function() {
+  masterVolume = rngVol.value
+}
+
 
 btnA.addEventListener(`click`, clickedA)
 btnB.addEventListener(`click`, clickedB)
 btnReplay.addEventListener(`click`, replayNotes)
+rngVol.addEventListener(`change`, updateVolume)
