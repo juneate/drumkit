5d9f427d0ace4359d5b53812d67cdb30679c03f3
Rocco <rp@Roccos-iMac.local>
2020-12-07 12:23:00 -0500

    Updated the drum kit SVG and finished off the ex

--- js/script.js
+++ js/script.js
@@ -1,53 +1,76 @@
-let notesPlayed = [] // An empty array (will hold our music)
-let masterVolume = 0.5
-let sfxFolder = `audio`
+let notesPlayed = [] // An empty array (will hold our music notes)
+let masterVolume = 0.5 // Default volume
+let sfxFolder = `audio` // Folder that all the audio clips are in
 
-// Select all playable drum parts
-let drumParts = document.querySelectorAll(`.playable`)
-let btnReplay = document.querySelector(`#replay`)
-let rngVol = document.querySelector(`#volume`)
+let drumParts = document.querySelectorAll(`.playable`) // Select all playable drum parts
+let btnReplay = document.querySelector(`#replay`) // Button
+let btnNewsong = document.querySelector(`#newsong`) // Button
+let rngVol = document.querySelector(`#volume`) // Volume
 
 // Runs once for each playable part of the drum kit
 let makesItPlayable = function(part) {
-  let theSfxFile = part.getAttribute(`data-sound`)
+  
+  // When we click on the playable drum part
   let playTheSound = function() {
-    let sound = new Audio(`${sfxFolder}/${theSfxFile}`)
-    sound.volume = masterVolume
-    sound.play()
+    // Get the sound file url
+    let theSfxFile = part.getAttribute(`data-sound`)
+
+    // Creates the new audio clip
+    let sound = new Audio(`${sfxFolder}/${theSfxFile}`) // Load the clip from a url
+    sound.volume = masterVolume // Set the volume
+    sound.play() // play it once
+
+    // Store it in the song history array
     notesPlayed.push(sound)
   }
   part.addEventListener(`click`, playTheSound)
 }
 
+// "Replay" button was pressed...
 let replayNotes = function() {
+
+  // If there is no instrument history
+  if (notesPlayed.length === 0) {
+    console.log(`There's no song to play!`)
+    return // leave immediately
+  }
+
   let index = 0  // counter
   
+  // Call this on a time delay, once for each note (like forEach, but slower)
 	let playOneNote = function() {
-    let note = notesPlayed[index]
-    let speed = document.querySelector(`#speed`).value
-
-		note.volume = masterVolume
+    let note = notesPlayed[index] // The note saved in the Array
+    
+		note.volume = masterVolume // Set the volume
     note.play() // Play the audio for the note
 
-    index = index + 1
+    index = index + 1 // Increase the counter for next time
 
-    // Only if there are still notes to play
+    // Only if there are still notes to play...
     if (index < notesPlayed.length) {
-      // Play a function at a 500ms delay
-      setTimeout(playOneNote, speed) // Loop this function again, in one second
+      // Figure out what delay is required (default is 500ms)
+      let speed = document.querySelector(`#speed`).value  //
+      // Play a function at a Xms delay
+      setTimeout(playOneNote, speed) // Loop this function again, in Xms
     }
   }
 
-  playOneNote() // just like a forEach, except it's timed
-	//notesPlayed.forEach(playOneNote)
+  playOneNote() // just like a forEach, except it's timed. This kicks off a loop of the function above
+	//notesPlayed.forEach(playOneNote)  // Would play all the sounds immediately (overlapping!)
+}
+
+let clearNotes = function() {
+  // Reset the history by assining a new empty array
+  notesPlayed = []
 }
 
 let updateVolume = function() {
+  // Change the master volume when the slider changes
   masterVolume = rngVol.value
 }
 
-
 // For each of the parts, call makeItPlayable, passing the item reference
 drumParts.forEach(makesItPlayable)
-btnReplay.addEventListener(`click`, replayNotes)
-rngVol.addEventListener(`change`, updateVolume)
+btnReplay.addEventListener(`click`, replayNotes) // Replay button
+btnNewsong.addEventListener(`click`, clearNotes) // Clear button
+rngVol.addEventListener(`change`, updateVolume) // Volume slide was updated
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Drum Kit example
+
+## Other features
+A few things that can be added to make this more fun and functional:
+- Disable all clickables when playback has started
+- Make the "play" button into a "stop" button while playing
+- Re-animate each part as its played back
+- Display volume as a numerical value
+- Display speed as BPM (beats per minute)
+- Show the current note queue
+   - Allow dragging the notes to change the order
+   - Allow deleting notes from history if we make a mistake
+- Create multiple layers of sound so two can play at once
+   - Perhaps could also assign the notes to a time signature as well
+   - Can custom add notes to any spot on the playback grid
